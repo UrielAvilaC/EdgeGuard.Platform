ad70c0a5fc6bd16e9376b36b15736e39ea0d383d
Uriel Avila <99363080+UrielAvilaC@users.noreply.github.com>
2026-05-16 17:24:29 -0600

    Add wwwroot to .gitignore

--- EdgeGuard.Platform/src/backend/Dicom.Edge.Hub.Api/wwwroot/chunk-FCLPKTVM.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-HIUAR4UN.js";var o=[{path:"",loadComponent:()=>import("./chunk-KZD4PNEC.js")},{path:":id",loadComponent:()=>import("./chunk-DWVJOXVC.js")}];export{o as patientsRoutes};
